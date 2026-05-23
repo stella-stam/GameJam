130ef53e66da364ee9a4ac39361e6925fa6390e7
stella-stam <stellastam@proton.me>
2026-05-23 14:03:36 +0300

    inside border collisions with player

--- Player.cs
+++ Player.cs
@@ -1,38 +1,31 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
+public partial class Player : CharacterBody2D
 {
 	[Export]
 	public int Speed { get; set;} = 400;
 
 	public Vector2 ScreenSize;
 
-	private Borders borders;
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
-
-		borders = GetNode<Borders>("../Borders");
-
-		GD.Print("Borders: ", borders.Position, borders.Size);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		handle_input_movement(delta);
+
 	}
 
 	private void handle_input_movement(double delta)
 	{
-		var velocity = Vector2.Zero; // The player's movement vector.
+		var velocity = Vector2.Zero;
 
 		if (Input.IsActionPressed("right"))
 		{
-			GD.Print("right pressed");
 			velocity.X += 1;
 		}
 
@@ -50,13 +43,16 @@ public partial class Player : Node2D
 		{
 			velocity.Y -= 1;
 		}
-		
-		Position += velocity * (float)delta * Speed;
 
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, borders.Position.X, borders.Position.X + borders.Size.X),
-			y: Mathf.Clamp(Position.Y, borders.Position.Y, borders.Position.Y + borders.Size.Y)
-		);
+		velocity = velocity.Normalized();
 
+		Velocity = velocity * Speed;
+
+		MoveAndSlide();
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		handle_input_movement(delta);
 	}
 }
